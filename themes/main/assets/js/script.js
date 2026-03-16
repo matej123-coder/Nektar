a96c3899e37e8366fab9dff0c69dea2130103c4b
matej123-coder <mmatej.minoski@gmail.com>
2026-03-16 15:58:35 +0100

    feat(web): setting up the default values

---
+++ themes/main/assets/js/script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded",()=>{
+    
+    gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+   const smoother = ScrollSmoother.create({
+    wrapper:"#wrapper",
+    content:"#content",
+    smooth:2,
+    smoothTouch:0.2,
+   })
+})
