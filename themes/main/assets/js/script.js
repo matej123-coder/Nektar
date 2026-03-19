7e2577927a8256065c73b7d07cb254a0a6ad9eb8
matej123-coder <mmatej.minoski@gmail.com>
2026-03-19 16:15:46 +0100

    feat: styling the navbar

--- themes/main/assets/js/script.js
+++ themes/main/assets/js/script.js
@@ -8,4 +8,25 @@ document.addEventListener("DOMContentLoaded",()=>{
     smooth:2,
     smoothTouch:0.2,    
    })
+    const navTrigger = document.querySelector(".dropdown-toggle");
+    const overlay = document.querySelector(".overlay");
+    const dropdown = document.querySelector(".dropdown");
+   
+    
+    dropdown.addEventListener("show.bs.dropdown", () => {
+        navTrigger.classList.add("nav-open");
+        overlay.classList.add("active");
+    });
+
+    dropdown.addEventListener("hide.bs.dropdown", () => {
+        navTrigger.classList.remove("nav-open");
+        overlay.classList.remove("active");
+    });
+    
+    document.querySelectorAll(".dropdown-item").forEach(item => {
+        item.addEventListener("click", () => {
+            navTrigger.classList.remove("nav-open");
+            overlay.classList.remove("active");
+        });
+    });
 })
